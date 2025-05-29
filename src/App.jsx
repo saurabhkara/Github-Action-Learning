4ec9b7ea1f8457baf5ca6ff75cbd9f18f4379160
Saurabh <saurabkumar679@gmail..com>
2025-05-29 19:45:12 +0530

    project intialized

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import Home from "./screens/Home";
+import About from "./screens/About";
+import Contact from "./screens/Contact";
+import Country from "./screens/Country";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "country",
+        element: <Country />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
